e33abba522b4f4a6141fc9efd0c30cb52e944aa0
André Dienes Friedrich <andre.dienes@gmail.com>
2022-08-08 13:10:57 -0300

    Atualização e criação de arquivos

---
+++ Mod11/cypress/e2e/pre-cadastro.spec.cy.js
@@ -0,0 +1,19 @@
+/// <reference types="cypress" />
+var faker = require('faker');
+
+describe('Funcionalidade pré Cadastro', () => {
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+    it('Deve completar com sucesso', () => {
+        cy.get('#reg_email').type(faker.internet.email())
+        cy.get('#reg_password').type('teste@teste.com')
+        cy.get(':nth-child(4) > .button').click()
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
+        cy.get('#account_first_name').type(faker.name.firstName())
+        cy.get('#account_last_name').type(faker.name.lastName())
+        cy.get('.woocommerce-Button').click()
+        cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso.')
+    });
+
+});
