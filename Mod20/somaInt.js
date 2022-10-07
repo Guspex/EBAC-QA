0ba7d238302c32e7a7c0ec26aac92de059f189b5
André Dienes Friedrich <andre.dienes@gmail.com>
2022-10-07 12:53:54 -0300

    Atualização para função

--- Mod20/somaInt.js
+++ Mod20/somaInt.js
@@ -1,7 +1,11 @@
 let totalDaSoma = 0
+function getTotalDaSoma() {
 for (var i = 0; i < 1000; i++) {
     if (i % 5 == 0 || i % 7 == 0) {
         totalDaSoma += i;
     }
 }
-console.log(totalDaSoma)
+
+return totalDaSoma
+}
+console.log(getTotalDaSoma())
