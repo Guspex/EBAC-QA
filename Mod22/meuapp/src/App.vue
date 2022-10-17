53927d34a57381351710137ba8aeb06cafdccdd5
André Dienes Friedrich <andre.dienes@gmail.com>
2022-10-17 14:08:43 -0300

    Atualização Mod22

---
+++ Mod22/meuapp/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <p>Meu Paragrafo</p>
+    <blockquote>
+      Minha Citação
+    </blockquote>
+    <details>
+      <summary>Meu Resumo</summary>
+      <p>Meu conteudo</p>
+    </details>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+#app {
+  font-family: Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #47a74f;
+  margin-top: 60px;
+}
+</style>
+
