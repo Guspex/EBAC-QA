d2ea4ab8e910c44eba06208f403ae3e880b21633
André Dienes Friedrich <andre.friedrich@bplus.com.br>
2023-04-10 09:04:09 -0300

    Atualização Mod22 para EXER Mod26

--- Mod12/cypress.config.js
+++ Mod12/cypress.config.js
@@ -2,6 +2,14 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: 'cdkv8i',
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/results',
+    overwrite: false,
+    html: true,
+    json: true
+  },
+  video: false,
   e2e: {
     "baseUrl": "http://lojaebac.ebaconline.art.br"
  
--- Mod22/cypress.config.js
+++ Mod22/cypress.config.js
@@ -1,11 +1,19 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/results',
+    overwrite: false,
+    html: true,
+    json: true
+  },
+  video: false,
   e2e: {
     setupNodeEvents(on, config) {
       
     },
-    baseUrl: 'http://lojaebac.ebaconline.art.br',
+    baseUrl: 'http://localhost:3000/',
     specPattern: "**/*.feature",
     specPattern: "**/**.cy.js"
   },  
