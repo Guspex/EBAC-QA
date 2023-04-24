ad51b51e74cf6c0eebdc9adcbaf9d8174723a10f
André Dienes Friedrich <andre.friedrich@bplus.com.br>
2023-04-24 10:04:09 -0300

    Atualizações modulos

--- Mod27/sample-quickstart-selenium-js-docker/sample-quickstart-selenium-js-docker/test/specs/lojaEbac.e2e.js
+++
@@ -1,9 +0,0 @@
-const homePage = require("../pageobjects/home.page")
-describe('Loja EBAC', () => {
-    it('should access home page', async () => {
-        await homePage.open()
-        expect(await homePage.title).toBe("EBAC – Shop – Página de teste")
-    });
-});
-
-
