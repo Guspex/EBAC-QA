6fcd8aac2d70ebfc0d93cc6c6cc2325ca19f849e
André Dienes Friedrich <andre.friedrich@bplus.com.br>
2023-05-03 15:55:31 -0300

    Atualização Mod30

---
+++ Mod30/configs/report.conf.js
@@ -0,0 +1,39 @@
+require ('dotenv').config()
+const allure = require('allure-commandline')
+const video = require('wdio-video-reporter')
+
+let reporterConf = process.env.REPORT === true ? {
+    reporters: ['spec',
+        ['allure', {
+            outputDir: 'allure-results',
+            disableWebdriverStepsReporting: true,
+            disableWebdriverScreenshotsReporting: true,
+        }],
+        [video, {
+            saveAllVideos: true,       // If true, also saves videos for successful test cases
+            videoSlowdownMultiplier: 50, // Higher to get slower videos, lower for faster videos [Value 1-100]
+        }]
+    ],
+    onComplete: function () {
+        const reportError = new Error('Could not generate Allure report')
+        const generation = allure(['generate', 'allure-results', '--clean'])
+        return new Promise((resolve, reject) => {
+            const generationTimeout = setTimeout(
+                () => reject(reportError),
+                5000)
+
+            generation.on('exit', function (exitCode) {
+                clearTimeout(generationTimeout)
+
+                if (exitCode !== 0) {
+                    return reject(reportError)
+                }
+
+                console.log('Allure report successfully generated')
+                resolve()
+            })
+        })
+    }
+} : {}
+
+module.exports = {reporterConf}
